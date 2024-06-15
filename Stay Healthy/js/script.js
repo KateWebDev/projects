76eb39c7d41fc46e1f8c37e097ef1e04e867853d
KateWebDev <5nh4bni8id2v@mail.ru>
2024-06-15 16:01:21 +0600

    Адаптивный сайт stay healthy - с адаптивом

---
+++ Stay Healthy/js/script.js
@@ -0,0 +1,55 @@
+document.addEventListener("click", (evt) => {
+  if (evt.target.closest(".header__burger")) {
+    document.querySelector(".menu").classList.toggle("menu--active");
+    document.querySelector("body").classList.toggle("lock");
+    document.querySelector(".header__burger").classList.toggle("header__burger--active");
+  } else if (!evt.target.closest(".arrow")) {
+    document.querySelector(".menu").classList.remove("menu--active");
+    document.querySelector("body").classList.remove("lock");
+    document.querySelector(".header__burger").classList.remove("header__burger--avtive");
+  }
+});
+
+document.addEventListener("keyup", (evt) => {
+  if (evt.key === "Escape") {
+    document.querySelector(".menu").classList.remove("menu--active");
+    document.querySelector("body").classList.remove("lock");
+    document.querySelector(".header__burger").classList.remove("header__burger--active");
+  }
+});
+
+document.addEventListener("click", (evt) => {
+  if (evt.target.closest(".menu__link")) {
+    let links = document.querySelectorAll(".menu__link");
+    links.forEach((link) => {
+      link.classList.remove("menu__link--active");
+    });
+    evt.target.classList.add("menu__link--active");
+    document.querySelector(".header__burger").classList.remove("header__burger--active");
+  }
+  if (evt.target.closest(".up") || evt.target.closest(".logo__img")) {
+    let links = document.querySelectorAll(".menu__link");
+    links.forEach((link) => {
+      link.classList.remove("menu__link--active");
+    });
+    document.querySelectorAll(".menu__link")[0].classList.add("menu__link--active");
+  }
+});
+
+let swiperPrograms = new Swiper(".programs__slider", {
+  navigation: {
+    prevEl: ".programs__button--prev",
+    nextEl: ".programs__button--next",
+  },
+  pagination: {
+    el: ".programs__pagination",
+    clickable: true,
+  },
+  slidesPerGroup: 1,
+  spaceBetween: 30,
+  breakpoints: {
+    320: { slidesPerView: 1 },
+    520: { slidesPerView: 2 },
+    991: { slidesPerView: 3 },
+  },
+});
