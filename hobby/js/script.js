685022edfd0042d1299435399942dbe5fea489f4
KateWebDev <5nh4bni8id2v@mail.ru>
2025-02-25 20:42:15 +0600

    Адаптивная верстка макета hobby

---
+++ hobby/js/script.js
@@ -0,0 +1,121 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+  // header fixed
+  const header = document.querySelector(".header");
+  const up = document.querySelector(".up");
+  const heightHeader = header.offsetHeight;
+  document.addEventListener("scroll", () => {
+    if (window.pageYOffset > heightHeader) {
+      if (header) {
+        header.classList.add("header--fixed");
+      }
+      if (up) {
+        up.classList.add("up--active");
+      }
+    } else {
+      if (header) {
+        header.classList.remove("header--fixed");
+      }
+      if (up) {
+        up.classList.remove("up--active");
+      }
+    }
+  });
+  //Бургер
+  document.addEventListener("click", (evt) => {
+    if (evt.target.closest(".header__burger")) {
+      if (document.querySelector(".menu")) {
+        document.querySelector(".menu").classList.toggle("menu--active");
+      }
+      if (document.querySelector("body")) {
+        document.querySelector("body").classList.toggle("lock");
+      }
+      if (document.querySelector(".header__burger")) {
+        document.querySelector(".header__burger").classList.toggle("header__burger--active");
+      }
+    } else if (!evt.target.closest(".arrow")) {
+      if (document.querySelector(".menu")) {
+        document.querySelector(".menu").classList.remove("menu--active");
+      }
+      if (document.querySelector(".submenu__items")) {
+        document.querySelector(".submenu__items").classList.remove("submenu__items--active");
+        document.querySelector(".submenu__items").classList.remove("submenu__items--open");
+      }
+      if (document.querySelector("body")) {
+        document.querySelector("body").classList.remove("lock");
+      }
+      if (document.querySelector(".header__burger")) {
+        document.querySelector(".header__burger").classList.remove("header__burger--active");
+      }
+    } else if (evt.target.closest(".menu__link")) {
+      let links = document.querySelectorAll(".menu__link");
+      links.forEach((link) => {
+        link.classList.remove("menu__link--active");
+      });
+      evt.target.classList.add("menu__link--active");
+      if (document.querySelector(".header__burger")) {
+        document.querySelector(".header__burger").classList.remove("header__burger--active");
+      }
+    } else if (evt.target.closest(".submenu__link")) {
+      if (document.querySelector(".header__burger")) {
+        document.querySelector(".header__burger").classList.remove("header__burger--active");
+      }
+    } else if (evt.target.closest(".up") || evt.target.closest(".logo__img")) {
+      let links = document.querySelectorAll(".menu__link");
+      links.forEach((link) => {
+        link.classList.remove("menu__link--active");
+      });
+      if (document.querySelectorAll(".menu__link").length > 0) {
+        document.querySelectorAll(".menu__link")[0].classList.add("menu__link--active");
+      }
+    } else if (evt.target.closest(".header__btn")) {
+      evt.preventDefault();
+      if (document.querySelector("body")) {
+        document.querySelector("body").classList.toggle("lock");
+      }
+      if (document.querySelector(".modal")) {
+        document.querySelector(".modal").classList.toggle("modal--active");
+      }
+    } else if (evt.target.closest(".modal__close")) {
+      if (document.querySelector("body")) {
+        document.querySelector("body").classList.remove("lock");
+      }
+      if (document.querySelector(".modal")) {
+        document.querySelector(".modal").classList.remove("modal--active");
+      }
+    }
+  });
+  //закрытие по нажатию кнопки esc
+  document.addEventListener("keyup", (evt) => {
+    if (evt.key === "Escape") {
+      if (document.querySelector(".menu")) {
+        document.querySelector(".menu").classList.remove("menu--active");
+      } else if (document.querySelector(".submenu__items")) {
+        document.querySelector(".submenu__items").classList.remove("submenu__items--active");
+        document.querySelector(".submenu__items").classList.remove("submenu__items--open");
+      } else if (document.querySelector(".body")) {
+        document.querySelector("body").classList.remove("lock");
+      } else if (document.querySelector(".close")) {
+        document.querySelector(".close").classList.remove("close--active");
+      } else if (document.querySelector(".header__burger")) {
+        document.querySelector(".header__burger").classList.remove("header__burger--active");
+      }
+    }
+  });
+  //Звездный рейтинг
+  function showRaiting(className, widthStars) {
+    $(`${className}`).rateYo({
+      numStars: 5,
+      starWidth: `${widthStars}px`,
+      normalFill: "#c4c4c4",
+      ratedFill: "#ffd700",
+      readOnly: true,
+      spacing: "6px",
+      fullStar: true,
+    });
+  }
+  if (document.querySelector(".products__raiting")) {
+    showRaiting(".products__raiting", 19);
+  }
+});
