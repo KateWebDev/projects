76bb853fc80bbfdd7e01e65172d5161be2f240cb
KateWebDev <5nh4bni8id2v@mail.ru>
2025-08-22 12:04:51 +0600

    designers portfolio

---
+++ designer portfolio/app/js/script.js
@@ -0,0 +1,135 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.addEventListener("click", (evt) => {
+    if (evt.target.closest(".header__burger")) {
+      if (document.querySelector(".menu")) {
+        document.querySelector(".menu").classList.toggle("menu--active");
+      }
+      if (document.querySelector("body")) {
+        document.querySelector("body").classList.toggle("lock");
+      }
+      if (document.querySelector(".header__burger")) {
+        document.querySelector(".header__burger").classList.toggle("header__burger--active");
+      }
+    } else if (!evt.target.closest(".arrow")) {
+      if (document.querySelector(".menu")) {
+        document.querySelector(".menu").classList.remove("menu--active");
+      }
+      if (document.querySelector(".submenu__items")) {
+        document.querySelector(".submenu__items").classList.remove("submenu__items--active");
+        document.querySelector(".submenu__items").classList.remove("submenu__items--open");
+      }
+      if (document.querySelector("body")) {
+        document.querySelector("body").classList.remove("lock");
+      }
+      if (document.querySelector(".header__burger")) {
+        document.querySelector(".header__burger").classList.remove("header__burger--active");
+      }
+    } else if (evt.target.closest(".menu__link")) {
+      let links = document.querySelectorAll(".menu__link");
+      links.forEach((link) => {
+        link.classList.remove("menu__link--active");
+      });
+      evt.target.classList.add("menu__link--active");
+      if (document.querySelector(".header__burger")) {
+        document.querySelector(".header__burger").classList.remove("header__burger--active");
+      }
+    } else if (evt.target.closest(".submenu__link")) {
+      if (document.querySelector(".header__burger")) {
+        document.querySelector(".header__burger").classList.remove("header__burger--active");
+      }
+    } else if (evt.target.closest(".up") || evt.target.closest(".logo__img")) {
+      let links = document.querySelectorAll(".menu__link");
+      links.forEach((link) => {
+        link.classList.remove("menu__link--active");
+      });
+      if (document.querySelectorAll(".menu__link").length > 0) {
+        document.querySelectorAll(".menu__link")[0].classList.add("menu__link--active");
+      }
+    } else if (evt.target.closest(".header__btn")) {
+      evt.preventDefault();
+      if (document.querySelector("body")) {
+        document.querySelector("body").classList.toggle("lock");
+      }
+      if (document.querySelector(".modal")) {
+        document.querySelector(".modal").classList.toggle("modal--active");
+      }
+    } else if (evt.target.closest(".modal__close")) {
+      if (document.querySelector("body")) {
+        document.querySelector("body").classList.remove("lock");
+      }
+      if (document.querySelector(".modal")) {
+        document.querySelector(".modal").classList.remove("modal--active");
+      }
+    }
+  });
+  document.addEventListener("keyup", (evt) => {
+    if (evt.key === "Escape") {
+      if (document.querySelector(".menu")) {
+        document.querySelector(".menu").classList.remove("menu--active");
+      } else if (document.querySelector(".submenu__items")) {
+        document.querySelector(".submenu__items").classList.remove("submenu__items--active");
+        document.querySelector(".submenu__items").classList.remove("submenu__items--open");
+      } else if (document.querySelector(".body")) {
+        document.querySelector("body").classList.remove("lock");
+      } else if (document.querySelector(".close")) {
+        document.querySelector(".close").classList.remove("close--active");
+      } else if (document.querySelector(".header__burger")) {
+        document.querySelector(".header__burger").classList.remove("header__burger--active");
+      }
+    }
+  });
+  if (document.querySelector(".portfolio__slider")) {
+    const swiper = new Swiper(".portfolio__slider", {
+      pagination: {
+        el: ".portfolio .pagination",
+        clickable: true,
+      },
+      keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+        pageUpDown: true,
+      },
+      slidesPerGroup: 1,
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+        },
+      },
+    });
+  }
+  if (document.querySelector(".testimonials__slider")) {
+    const swiper = new Swiper(".testimonials__slider", {
+      pagination: {
+        el: ".testimonials .pagination",
+        clickable: true,
+      },
+      keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+        pageUpDown: true,
+      },
+      autoHeight: false,
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      effect: "fade",
+      fadeEffect: {
+        crossFade: true,
+      },
+    });
+  }
+  document.querySelectorAll("textarea").forEach((el) => {
+    el.style.height = el.setAttribute("style", "height: 26px");
+    el.classList.add("auto");
+    el.addEventListener("input", (e) => {
+      el.style.height = "auto";
+      el.style.height = el.scrollHeight + "px";
+    });
+  });
+});
