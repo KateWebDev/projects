f51b426a878ef33e275d72ff6493bcb2480376b3
KateWebDev <5nh4bni8id2v@mail.ru>
2025-09-23 22:36:42 +0600

    верстка макета dm leasing

---
+++ lease cars/js/script.js
@@ -0,0 +1,165 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+  // header fixed
+  const header = document.querySelector(".header");
+  const up = document.querySelector(".up");
+  const heightHeader = header.offsetHeight;
+  document.addEventListener("scroll", () => {
+    if (window.pageYOffset > heightHeader) {
+      if (header) {
+        header.classList.add("header--fixed");
+      }
+      if (up) {
+        up.classList.add("up--active");
+      }
+    } else {
+      if (header) {
+        header.classList.remove("header--fixed");
+      }
+      if (up) {
+        up.classList.remove("up--active");
+      }
+    }
+  });
+  //Бургер
+  document.addEventListener("click", (evt) => {
+    if (evt.target.closest(".header__burger")) {
+      if (document.querySelector(".menu")) {
+        document.querySelector(".menu").classList.toggle("menu--active");
+      }
+      if (document.querySelector("body")) {
+        document.querySelector("body").classList.toggle("lock");
+      }
+      if (document.querySelector(".header__burger")) {
+        document.querySelector(".header__burger").classList.toggle("header__burger--active");
+      }
+    } else if (!evt.target.closest(".arrow")) {
+      if (document.querySelector(".menu")) {
+        document.querySelector(".menu").classList.remove("menu--active");
+      }
+      if (document.querySelector(".submenu__items")) {
+        document.querySelector(".submenu__items").classList.remove("submenu__items--active");
+        document.querySelector(".submenu__items").classList.remove("submenu__items--open");
+      }
+      if (document.querySelector("body")) {
+        document.querySelector("body").classList.remove("lock");
+      }
+      if (document.querySelector(".header__burger")) {
+        document.querySelector(".header__burger").classList.remove("header__burger--active");
+      }
+    } else if (evt.target.closest(".menu__link")) {
+      let links = document.querySelectorAll(".menu__link");
+      links.forEach((link) => {
+        link.classList.remove("menu__link--active");
+      });
+      evt.target.classList.add("menu__link--active");
+      if (document.querySelector(".header__burger")) {
+        document.querySelector(".header__burger").classList.remove("header__burger--active");
+      }
+    } else if (evt.target.closest(".submenu__link")) {
+      if (document.querySelector(".header__burger")) {
+        document.querySelector(".header__burger").classList.remove("header__burger--active");
+      }
+    } else if (evt.target.closest(".up") || evt.target.closest(".logo__img")) {
+      let links = document.querySelectorAll(".menu__link");
+      links.forEach((link) => {
+        link.classList.remove("menu__link--active");
+      });
+      if (document.querySelectorAll(".menu__link").length > 0) {
+        document.querySelectorAll(".menu__link")[0].classList.add("menu__link--active");
+      }
+    } else if (evt.target.closest(".header__btn")) {
+      evt.preventDefault();
+      if (document.querySelector("body")) {
+        document.querySelector("body").classList.toggle("lock");
+      }
+      if (document.querySelector(".modal")) {
+        document.querySelector(".modal").classList.toggle("modal--active");
+      }
+    } else if (evt.target.closest(".modal__close")) {
+      if (document.querySelector("body")) {
+        document.querySelector("body").classList.remove("lock");
+      }
+      if (document.querySelector(".modal")) {
+        document.querySelector(".modal").classList.remove("modal--active");
+      }
+    }
+  });
+  //закрытие по нажатию кнопки esc
+  document.addEventListener("keyup", (evt) => {
+    if (evt.key === "Escape") {
+      if (document.querySelector(".menu")) {
+        document.querySelector(".menu").classList.remove("menu--active");
+      } else if (document.querySelector(".submenu__items")) {
+        document.querySelector(".submenu__items").classList.remove("submenu__items--active");
+        document.querySelector(".submenu__items").classList.remove("submenu__items--open");
+      } else if (document.querySelector(".body")) {
+        document.querySelector("body").classList.remove("lock");
+      } else if (document.querySelector(".close")) {
+        document.querySelector(".close").classList.remove("close--active");
+      } else if (document.querySelector(".header__burger")) {
+        document.querySelector(".header__burger").classList.remove("header__burger--active");
+      }
+    }
+  });
+
+  // Табы
+  let tabs = document.querySelectorAll(".tabs__item");
+  let tabsContents = document.querySelectorAll(".tabs__block");
+
+  for (let i = 0; i < tabs.length; i++) {
+    //
+    tabs[i].addEventListener("click", (evt) => {
+      evt.preventDefault();
+      tabs.forEach((tab) => {
+        tab.classList.remove("tabs__item--active");
+      });
+      tabs[i].classList.add("tabs__item--active");
+
+      tabsContents.forEach((tabsContent) => {
+        tabsContent.classList.remove("tabs__block--active");
+      });
+      tabsContents[i].classList.add("tabs__block--active");
+    });
+  }
+
+  // Автоматическая высота у textarea (увеличивается в зависимости от кол-ва введенного текста)
+  document.querySelectorAll("textarea").forEach((el) => {
+    el.style.height = el.setAttribute("style", "height: " + el.scrollHeight + "px");
+    el.classList.add("auto");
+    el.addEventListener("input", (e) => {
+      el.style.height = "auto";
+      el.style.height = el.scrollHeight + "px";
+    });
+  });
+
+  // slider swiper
+  if (document.querySelector(".intro__slider")) {
+    const swiper = new Swiper(".intro__slider", {
+      pagination: {
+        el: ".slider__pagination",
+        clickable: true,
+      },
+      keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+        pageUpDown: true,
+      },
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      loop: true,
+      autoplay: {
+        // Задержка
+        delay: 5000,
+        stopOnLastSlide: true,
+        disableOnInteraction: false,
+      },
+      speed: 500,
+      effect: "fade",
+      fadeEffect: {
+        crossFade: false,
+      },
+    });
+  }
+});
