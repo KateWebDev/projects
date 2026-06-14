bb1555277ead2a2b24ae4294fba59f7bb3e5be11
KateWebDev <5nh4bni8id2v@mail.ru>
2026-06-14 11:38:55 +0600

    project 9 - memo, useMemo, useCallback

--- react in study Jonas Schmedtmann/12-atomic-blog/starter/src/App-memo.js
+++ react in study Jonas Schmedtmann/12-atomic-blog/starter/src/App-memo.js
@@ -1,10 +1,33 @@
-import { useState, useEffect } from "react";
-import { PostProvider } from "./PostContext";
-import { usePostContext } from "./usePostContext";
-// import Test from "./Test";
+import { memo, useCallback, useEffect, useMemo, useState } from "react";
+import { faker } from "@faker-js/faker";
+
+function createRandomPost() {
+  return {
+    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+    body: faker.hacker.phrase(),
+  };
+}
 
 function App() {
+  const [posts, setPosts] = useState(() => Array.from({ length: 30 }, () => createRandomPost()));
+  const [searchQuery, setSearchQuery] = useState("");
   const [isFakeDark, setIsFakeDark] = useState(false);
+
+  // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) => `${post.title} ${post.body}`.toLowerCase().includes(searchQuery.toLowerCase()))
+      : posts;
+
+  const handleAddPost = useCallback(function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }, []);
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+
+  // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
   useEffect(
     function () {
       document.documentElement.classList.toggle("fake-dark-mode");
@@ -12,68 +35,75 @@ function App() {
     [isFakeDark],
   );
 
+  const archiveOptions = useMemo(() => {
+    return {
+      show: false,
+      title: `Post archive in addition to ${posts.length} main posts`,
+    };
+  }, [posts.length]);
+
   return (
     <section>
       <button onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)} className="btn-fake-dark-mode">
         {isFakeDark ? "☀️" : "🌙"}
       </button>
-      <PostProvider>
-        <Header />
-        <Main />
-        <Archive />
-        <Footer />
-      </PostProvider>
+
+      <Header
+        posts={searchedPosts}
+        onClearPosts={handleClearPosts}
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+      />
+      <Main posts={searchedPosts} onAddPost={handleAddPost} />
+      <Archive archiveOptions={archiveOptions} onAddPost={handleAddPost} setIsFakeDark={setIsFakeDark} />
+      <Footer />
     </section>
   );
 }
 
-function Header() {
-  const { onClearPosts } = usePostContext();
+function Header({ posts, onClearPosts, searchQuery, setSearchQuery }) {
   return (
     <header>
       <h1>
         <span>⚛️</span>The Atomic Blog
       </h1>
       <div>
-        <Results />
-        <SearchPosts />
+        <Results posts={posts} />
+        <SearchPosts searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
         <button onClick={onClearPosts}>Clear posts</button>
       </div>
     </header>
   );
 }
 
-function SearchPosts() {
-  const { searchQuery, setSearchQuery } = usePostContext();
+function SearchPosts({ searchQuery, setSearchQuery }) {
   return <input value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} placeholder="Search posts..." />;
 }
 
-function Results() {
-  const { posts } = usePostContext();
+function Results({ posts }) {
   return <p>🚀 {posts.length} atomic posts found</p>;
 }
 
-function Main() {
+function Main({ posts, onAddPost }) {
   return (
     <main>
-      <FormAddPost />
-      <Posts />
+      <FormAddPost onAddPost={onAddPost} />
+      <Posts posts={posts} />
     </main>
   );
 }
 
-function Posts() {
+function Posts({ posts }) {
   return (
     <section>
-      <List />
+      <List posts={posts} />
     </section>
   );
 }
 
-function FormAddPost() {
+function FormAddPost({ onAddPost }) {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
-  const { onAddPost } = usePostContext();
 
   const handleSubmit = function (e) {
     e.preventDefault();
@@ -92,35 +122,31 @@ function FormAddPost() {
   );
 }
 
-function List() {
-  const { posts } = usePostContext();
+function List({ posts }) {
   return (
-    <>
-      <ul>
-        {posts.map((post, i) => (
-          <li key={i}>
-            <h3>{post.title}</h3>
-            <p>{post.body}</p>
-          </li>
-        ))}
-      </ul>
-      {/* <Test /> */}
-    </>
+    <ul>
+      {posts.map((post, i) => (
+        <li key={i}>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+        </li>
+      ))}
+    </ul>
   );
 }
 
-function Archive() {
+const Archive = memo(function ({ archiveOptions, onAddPost, setIsFakeDark }) {
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
-  const { onAddPost, createRandomPost } = usePostContext();
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
-    Array.from({ length: 10000 }, () => createRandomPost()),
+    Array.from({ length: 40000 }, () => createRandomPost()),
   );
 
-  const [showArchive, setShowArchive] = useState(false);
+  const [showArchive, setShowArchive] = useState(archiveOptions.show);
+
   return (
     <aside>
-      <h2>Post archive</h2>
+      <h2>{archiveOptions.title}</h2>
       <button onClick={() => setShowArchive((s) => !s)}>
         {showArchive ? "Hide archive posts" : "Show archive posts"}
       </button>
@@ -139,7 +165,7 @@ function Archive() {
       )}
     </aside>
   );
-}
+});
 
 function Footer() {
   return <footer>&copy; by The Atomic Blog ✌️</footer>;
--- react in study Jonas Schmedtmann/12-atomic-blog/starter/src/Test.js
+++ react in study Jonas Schmedtmann/12-atomic-blog/starter/src/Test.js
@@ -23,4 +23,4 @@ export default function Test() {
       <SlowComponent />
     </div>
   );
-}
+} 
