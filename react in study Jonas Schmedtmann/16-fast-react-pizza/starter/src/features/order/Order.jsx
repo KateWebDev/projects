59906129c08f4dd4f0f3a58236a18af92277e2dc
KateWebDev <5nh4bni8id2v@mail.ru>
2026-06-17 12:21:08 +0600

    project 12 - file analysis and preparation

--- react in study Jonas Schmedtmann/16-fast-react-pizza/starter/src/features/order/Order.jsx
+++ react in study Jonas Schmedtmann/16-fast-react-pizza/starter/src/features/order/Order.jsx
@@ -1,10 +1,6 @@
 // Test ID: IIDSAT
 
-import {
-  calcMinutesLeft,
-  formatCurrency,
-  formatDate,
-} from "../../utils/helpers";
+import { calcMinutesLeft, formatCurrency, formatDate } from "../../utils/helpers";
 
 const order = {
   id: "ABCDEF",
@@ -43,15 +39,7 @@ const order = {
 
 function Order() {
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
-  const {
-    id,
-    status,
-    priority,
-    priorityPrice,
-    orderPrice,
-    estimatedDelivery,
-    cart,
-  } = order;
+  const { id, status, priority, priorityPrice, orderPrice, estimatedDelivery, cart } = order;
   const deliveryIn = calcMinutesLeft(estimatedDelivery);
 
   return (
@@ -67,9 +55,7 @@ function Order() {
 
       <div>
         <p>
-          {deliveryIn >= 0
-            ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
-            : "Order should have arrived"}
+          {deliveryIn >= 0 ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃` : "Order should have arrived"}
         </p>
         <p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
       </div>
