f45ccc7ce6441dd6965cb19567cd59bbfda1b63a
KateWebDev <5nh4bni8id2v@mail.ru>
2026-07-24 12:40:22 +0600

    project 13 - installation and initial configuration of React Query

--- react in study Jonas Schmedtmann/17-the-wild-oasis/starter/src/App.jsx
+++ react in study Jonas Schmedtmann/17-the-wild-oasis/starter/src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Account from "./pages/Account";
 import Booking from "./pages/Bookings";
 import Cabins from "./pages/Cabins";
@@ -10,9 +12,18 @@ import PageNotFound from "./pages/PageNotFound";
 import { GlobalStyles } from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000, //1 minute
+    },
+  },
+});
+
 export default function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -29,6 +40,6 @@ export default function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
