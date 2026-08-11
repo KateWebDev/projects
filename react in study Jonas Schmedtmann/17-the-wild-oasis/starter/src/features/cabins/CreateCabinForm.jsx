c48336fdeabd80b118ffcceac32a3e311819e614
KateWebDev <5nh4bni8id2v@mail.ru>
2026-08-11 21:29:34 +0600

    project 13 - validation of form fields using react-hook-form

--- react in study Jonas Schmedtmann/17-the-wild-oasis/starter/src/features/cabins/CreateCabinForm.jsx
+++ react in study Jonas Schmedtmann/17-the-wild-oasis/starter/src/features/cabins/CreateCabinForm.jsx
@@ -1,56 +1,21 @@
-import styled from "styled-components";
 import { useForm } from "react-hook-form";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-import { Input } from "../../ui/Input";
 import Form from "../../ui/Form";
+import { Input } from "../../ui/Input";
 import { Button } from "../../ui/Button";
 import FileInput from "../../ui/FileInput";
 import Textarea from "../../ui/Textarea";
 import { createCabin } from "../../services/apiCabins";
 import toast from "react-hot-toast";
-
-const FormRow = styled.div`
-  display: grid;
-  align-items: center;
-  grid-template-columns: 24rem 1fr 1.2fr;
-  gap: 2.4rem;
-
-  padding: 1.2rem 0;
-
-  &:first-child {
-    padding-top: 0;
-  }
-
-  &:last-child {
-    padding-bottom: 0;
-  }
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-
-  &:has(button) {
-    display: flex;
-    justify-content: flex-end;
-    gap: 1.2rem;
-  }
-`;
-
-const Label = styled.label`
-  font-weight: 500;
-`;
-
-const Error = styled.span`
-  font-size: 1.4rem;
-  color: var(--color-red-700);
-`;
+import FormRow from "../../ui/FormRow";
 
 function CreateCabinForm() {
   const {
     register,
     handleSubmit,
     reset,
+    getValues,
     formState: { errors },
   } = useForm();
 
@@ -75,38 +40,76 @@ function CreateCabinForm() {
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
-      <FormRow>
-        <Label htmlFor="name">Cabin name</Label>
-        <Input type="text" id="name" {...register("name")} />
+      <FormRow label="Cabin name" error={errors?.name?.message}>
+        <Input
+          type="text"
+          id="name"
+          {...register("name", {
+            required: "this field is required to fill in",
+          })}
+        />
+      </FormRow>
+
+      <FormRow label="Maximum capacity" error={errors?.maxCapacity?.message}>
+        <Input
+          type="number"
+          id="maxCapacity"
+          {...register("maxCapacity", {
+            required: "this field is required to fill in",
+            min: {
+              value: 1,
+              message: "Capacity must be greater than 1",
+            },
+          })}
+        />
+      </FormRow>
+
+      <FormRow label="Regular price" error={errors?.regularPrice?.message}>
+        <Input
+          type="number"
+          id="regularPrice"
+          {...register("regularPrice", {
+            required: "this field is required to fill in",
+            min: {
+              value: 1,
+              message: "Price must be greater than 1",
+            },
+          })}
+        />
+      </FormRow>
+
+      <FormRow label="Discount" error={errors?.discount?.message}>
+        <Input
+          type="number"
+          id="discount"
+          defaultValue={0}
+          {...register("discount", {
+            required: "this field is required to fill in",
+            max: {
+              value: 1000,
+              message: "Discount must be less than 1000",
+            },
+            validate: (value) => value <= getValues().regularPrice || "Discount is more than the price list",
+          })}
+        />
+      </FormRow>
+
+      <FormRow label="Description for website " error={errors?.description?.message}>
+        <Textarea
+          type="number"
+          id="description"
+          defaultValue=""
+          {...register("description", {
+            required: "this field is required to fill in",
+          })}
+        />
+      </FormRow>
+
+      <FormRow label="Cabin photo">
+        <FileInput id="image" accept="image/*" />
       </FormRow>
 
       <FormRow>
-        <Label htmlFor="maxCapacity">Maximum capacity</Label>
-        <Input type="number" id="maxCapacity" {...register("maxCapacity")} />
-      </FormRow>
-
-      <FormRow>
-        <Label htmlFor="regularPrice">Regular price</Label>
-        <Input type="number" id="regularPrice" {...register("regularPrice")} />
-      </FormRow>
-
-      <FormRow>
-        <Label htmlFor="discount">Discount</Label>
-        <Input type="number" id="discount" defaultValue={0} {...register("discount")} />
-      </FormRow>
-
-      <FormRow>
-        <Label htmlFor="description">Description for website</Label>
-        <Textarea type="number" id="description" defaultValue="" {...register("description")} />
-      </FormRow>
-
-      <FormRow>
-        <Label htmlFor="image">Cabin photo</Label>
-        <FileInput id="image" accept="image/*" {...register("image")} />
-      </FormRow>
-
-      <FormRow>
-        {/* type is an HTML attribute! */}
         <Button type="reset">Reset</Button>
         <Button disabled={isCreating}>Add cabin</Button>
       </FormRow>
