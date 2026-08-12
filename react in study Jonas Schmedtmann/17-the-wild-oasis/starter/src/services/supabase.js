a838f4aac98907a545e875436b890d280b2a6852
KateWebDev <5nh4bni8id2v@mail.ru>
2026-08-12 18:54:05 +0600

    project 13 - file uploads (adding an image for the cabin)

--- react in study Jonas Schmedtmann/17-the-wild-oasis/starter/src/services/supabase.js
+++ react in study Jonas Schmedtmann/17-the-wild-oasis/starter/src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://xvraznqijqwatknepfoc.supabase.co";
+export const supabaseUrl = "https://xvraznqijqwatknepfoc.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2cmF6bnFpanF3YXRrbmVwZm9jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQzODY3OTEsImV4cCI6MjA5OTk2Mjc5MX0.IdNqrAouVbO7NRiORKsM4q2FTTaYfTDwjbwNsupl3Yk";
 const supabase = createClient(supabaseUrl, supabaseKey);
